a00753725022f990a4868aece77dea30b100e622
Lisa Carlson <lisa.carlson85@gmail.com>
2015-11-09 21:00:33 -0700

    refactored to work with query strings

--- calc/app.js
+++ calc/app.js
@@ -6,24 +6,54 @@ app.config(function($routeProvider, $locationProvider) {
         templateUrl: 'partials/add.html',
         controller: 'AddController'
       })
+      .when('/add', {
+        templateUrl: 'partials/add.html',
+        controller: 'AddController'
+      })
+      .when('/div', {
+        templateUrl: 'partials/div.html',
+        controller: 'DivController'
+      })
       .when('/div/:num1/:num2', {
         templateUrl: 'partials/div.html',
         controller: 'DivController'
       })
-      .otherwise({
-        redirectTo: '/'
-      });
+      // .otherwise({
+      //   redirectTo: '/'
+      // });
     $locationProvider.html5Mode(true);
 });
 
-app.controller("GlobalController", function ($scope, $routeParams) {  
-  $scope.submit = "Home"
+app.controller("GlobalController", function ($scope, $routeParams, $location) {  
+  $scope.add = function(){
+    $location.url('/add/'+$scope.number1+'/'+$scope.number2);
+    $scope.number1 = '';
+    $scope.number2 = '';
+
+  }
+  $scope.div = function(){
+    $location.url('/div/'+$scope.number1+'/'+$scope.number2)
+    $scope.number1 = '';
+    $scope.number2 = '';
+  }
 });
 
-app.controller("AddController", function ($scope, $routeParams) {
-  $scope.result = Number($routeParams.num1) + Number($routeParams.num2);
+app.controller("AddController", function ($scope, $routeParams, $location) {
+  var query = $location.search();
+  if (Object.keys(query).length) {
+    $scope.result = Number(query.x)+Number(query.y);
+  }
+  else {
+    $scope.result = Number($routeParams.num1) + Number($routeParams.num2);
+  }
 });
 
-app.controller('DivController', function ($scope, $routeParams){
-  $scope.result = Number($routeParams.num1) / Number($routeParams.num2);
+app.controller('DivController', function ($scope, $routeParams, $location){
+  var query = $location.search();
+  if (Object.keys(query).length) {
+    $scope.result = Number(query.x)/Number(query.y);
+  }
+  else {
+    $scope.result = Number($routeParams.num1) / Number($routeParams.num2);
+  }
 });
